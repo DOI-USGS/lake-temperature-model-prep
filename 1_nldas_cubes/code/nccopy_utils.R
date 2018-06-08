6c410b23696f16a0bdb375b4ea8638361a86d921
Jordan S Read <jread@usgs.gov>
2018-06-07 20:50:41 -0500

    peer review fixes

--- 1_nldas_cubes/code/nccopy_utils.R
+++ 1_nldas_cubes/code/nccopy_utils.R
@@ -11,11 +11,11 @@ nccopy_nldas <- function(nc_filepath){
     stop(nldas_url, ' **FAILED nccopy**', call. = FALSE)
   }
   # check/verify file has the appropriate number of timesteps...fail if not
-  expected_steps <- length(nldad_steps_from_file(nc_filename))
+  expected_steps <- length(nldas_steps_from_file(nc_filename))
   nc <- ncdf4::nc_open( nc_filepath )
   nc_steps <- nc$dim$time$len
+  ncdf4::nc_close(nc)
   if (nc_steps != expected_steps){
-    ncdf4::nc_close(nc)
     unlink(nc_filename)
     stop('incomplete file ', nc_filename , '\nexpected ', 
          expected_steps, ' steps, found ', nc_steps, ' steps\nfor ', nldas_url, call. = FALSE)
@@ -23,7 +23,7 @@ nccopy_nldas <- function(nc_filepath){
   invisible()
 }
 
-nldad_steps_from_file <- function(nc_filename){
+nldas_steps_from_file <- function(nc_filename){
   file_chunks <- strsplit(nc_filename,'[_]')[[1]]
   time_range <- strsplit(file_chunks[2],'[.]')[[1]] %>% as.numeric
   return(seq(time_range[1], time_range[2]))
