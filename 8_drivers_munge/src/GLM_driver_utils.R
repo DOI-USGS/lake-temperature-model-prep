56eaf6369ac45df7b1c3a4f28241104fbb743162
Jordan S Read <jread@usgs.gov>
2018-08-08 19:19:43 -0500

    initial driver files

--- 8_drivers_munge/src/GLM_driver_utils.R
+++ 8_drivers_munge/src/GLM_driver_utils.R
@@ -39,7 +39,7 @@ create_driver_task_plan <- function(driver_files, feather_ind_files, data_dir, i
   driver_task_step <- create_task_step(
     step_name = 'munge_drivers',
     target = function(task_name, step_name, ...) {
-      file.path(data_dir, task_name)
+      file.path(out_dir, task_name)
     },
     command = function(target_name, task_name, ...) {
       this_time <- parse_meteo_filepath(task_name, 'time') # better filtering needed?
@@ -52,6 +52,8 @@ create_driver_task_plan <- function(driver_files, feather_ind_files, data_dir, i
     }
   )
 
+  out_dir <- dirname(driver_files) %>% unique()
+  stopifnot(length(out_dir) == 1) # more than one not supported with this pattern
   filenames <- basename(driver_files)
 
   create_task_plan(filenames, list(driver_task_step), final_steps='munge_drivers', ind_dir = ind_dir)
@@ -65,3 +67,42 @@ create_driver_task_makefile <- function(makefile, task_plan, include, packages, 
 
 }
 
+
+feathers_to_driver_file <- function(filepath, ..., dirname, feather_files = NULL){
+  if (is.null(feather_files)){
+    feather_inds <- c(...)
+    feather_filepaths <- file.path(dirname, scipiper::as_data_file(basename(feather_inds)))
+  }
+  NLDAS_start <- as.POSIXct('1979-01-01 13:00', tz = "UTC")
+  all_time_vec <- seq(NLDAS_start, by = 'hour', to = Sys.time())
+  file_times <- parse_meteo_filepath(filepath, 'time') + 1 # not zero indexed
+  drivers_in <- data.frame(time = all_time_vec[file_times[1]:file_times[2]], stringsAsFactors = FALSE)
+  for (feather_filepath in feather_filepaths){
+    message(feather_filepath)
+    data <- read_feather(feather_filepath)
+    drivers_in <- cbind(drivers_in, data)
+  }
+
+  # https://github.com/USGS-R/mda.lakes/blob/8e4925e63f1403e0802162437373fe62b243d024/R/get_driver_nhd.R
+  # turn into actual driver vars:
+  drivers_out <- drivers_in %>% rename(ShortWave = dswrfsfc, LongWave = dlwrfsfc) %>%
+    mutate(WindSpeed = sqrt(ugrd10m^2 + vgrd10m^2), AirTemp = tmp2m - 273.15,
+           RelHum = 100*spfh2m/qsat(AirTemp, pressfc*0.01),
+           Rain = apcpsfc*24/1000) %>%
+    mutate(Snow = ifelse(AirTemp < 0, Rain*10, 0), Rain = ifelse(AirTemp < 0, 0, Rain)) %>% #convert to m/day rate)
+    select(time, ShortWave, LongWave, AirTemp, RelHum, WindSpeed, Rain, Snow)
+
+  # now downsample?
+  readr::write_csv()
+}
+
+# from old MATLAB code:
+#    INPUT:   Ta - air temperature  [C]
+#             Pa - (optional) pressure [mb]
+#
+#    OUTPUT:  q  - saturation specific humidity  [kg/kg]
+qsat = function(Ta, Pa){
+  ew = 6.1121*(1.0007+3.46e-6*Pa)*exp((17.502*Ta)/(240.97+Ta)) # in mb
+  q  = 0.62197*(ew/(Pa-0.378*ew))                              # mb -> kg/kg
+  return(q)
+}
