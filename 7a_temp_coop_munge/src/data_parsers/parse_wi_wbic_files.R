3afd01de22c1f8e7cc7883b2729b9a58baafdbe0
Samantha Oliver <soliver@usgs.gov>
2018-09-06 14:00:12 -0500

    modified wi parser - some lakes had multiple obs per depth/day, but did not have time stamp. Solution was to select first temp observation for each lake/date/depth.

--- 7a_temp_coop_munge/src/data_parsers/parse_wi_wbic_files.R
+++ 7a_temp_coop_munge/src/data_parsers/parse_wi_wbic_files.R
@@ -8,11 +8,16 @@ parse_WI_Profile_Data_1995to2015 <- function(inind, outind) {
   # read in data
   raw_dat <- readxl::read_excel(infile, sheet = 'temperature')
 
-  # reformat/munge data
-  dat_clean <- raw_dat %>%
-    mutate(depth = ifelse(depth.units %in% 'f', depth/3.28, depth),
-           temp = ifelse(temp.units %in% "F", fahrenheit_to_celsius(temperature), temperature)) %>%
-    select(DateTime = date, depth, temp, WBIC)
+  # group by date and depth and see if there are duplicate values
+  # there are, but time stamp is not helping. Take first value of each lake/date/depth combination
+  dat_clean <- mutate(raw_dat, DateTime = as.Date(date),
+                     depth = ifelse(depth.units %in% 'f', depth/3.28, depth),
+                     temp = ifelse(temp.units %in% "F", fahrenheit_to_celsius(temperature), temperature)) %>%
+    group_by(DateTime, WBIC, depth) %>%
+    summarize(temp = first(temp)) %>%
+    select(DateTime, depth, temp, WBIC) %>%
+    filter(!is.na(depth), !is.na(temp)) %>%
+    arrange(WBIC, DateTime)
 
   saveRDS(object = dat_clean, file = outfile)
   sc_indicate(ind_file = outind, data_file = outfile)
