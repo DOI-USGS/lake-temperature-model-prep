2f444b1f57c133286f470bdb03328fe4066ab00f
jzwart <jzwart@usgs.gov>
2018-09-18 15:15:00 -0500

    moving function

--- 6_drivers/src/grid_utils.R
+++ 6_drivers/src/grid_utils.R
@@ -40,26 +40,6 @@ cells_containing_points <- function(cell_grid, points){
   return(cells_w_pts)
 }
 
-#' get the x and y indices of cells that correspond to each nhd_id
-#'
-#' @param cell_grid an `st_sf` of polygons with fields "x" and "y"
-#' @param points spatial points sharing the same CRS as `cell_grid`
-#'
-#' @return a data.frame with nhd_id that corresponds to the cell x and y fields
-nhd_id_to_cell_coords <- function(cell_grid, points){
-  which_cell <- st_intersects(points, cell_grid) # tells which NLDAS grid cell contains each point
-
-  #  all features are within grid cell
-  nhd_nldas_coords <- points %>%
-    mutate(nldas_cell_row = as.numeric(which_cell)) %>%
-    left_join(as_tibble(cell_grid) %>% mutate(nldas_cell_row = as.numeric(row.names(cell_grid))) %>% select(x, y, nldas_cell_row),
-              by = 'nldas_cell_row') %>%
-    select(-nldas_cell_row) %>%
-    rename(nldas_coord_x = x, nldas_coord_y = y)
-
-  return(nhd_nldas_coords)
-}
-
 
 sf_file_centroids <- function(filepath){
 
