0b5db6cb905d38b51b9ec20396c1ef0e9c8650db
Jordan S Read <jread@usgs.gov>
2018-12-01 14:41:56 -0600

    example profile stacked plots

--- 6_drivers/src/local_meteo_utils.R
+++ 6_drivers/src/local_meteo_utils.R
@@ -166,6 +166,7 @@ buoy_data <- function(){
            sampledate < as.Date('2009-07-03') | sampledate > as.Date('2009-07-06'),
            sampledate < as.Date('2014-06-17') | sampledate > as.Date('2014-07-10'),
            sampledate < as.Date('2008-11-11') | sampledate > as.Date('2008-12-12'),
+           sampledate < as.Date('2014-07-06') | sampledate > as.Date('2014-10-20'),
            sampledate < as.Date('2011-08-07') | sampledate > as.Date('2011-08-19'),
            sampledate < as.Date('2012-08-31') | sampledate > as.Date('2012-09-04')) %>%
     select(DateTime = sampledate, Depth = depth, temp = wtemp)
@@ -179,23 +180,26 @@ buoy_data <- function(){
     mutate(temp = ifelse(is.na(temp.x), temp.y, temp.x)) %>% select(-temp.x, -temp.y) %>%
     arrange(DateTime, Depth)
 
+  incomplete_profiles <- combined %>% group_by(DateTime) %>% tally() %>% filter(n < 3) %>% pull(DateTime)
+
+  combined <- combined %>% filter(!DateTime %in% incomplete_profiles, DateTime > as.Date('2009-04-01'))
   readr::write_csv(buoy_temp, '../lake_modeling/data_imports/in/mendota_buoy.csv')
   readr::write_csv(combined, '../lake_modeling/data_imports/in/mendota_combined.csv')
   return(combined)
 }
 
-build_training_datasets <- function(n_experiments = 5, samples = c(800, 100, 50, 20, 10, 5, 2), training_range = as.Date(c('2009-04-01', '2013-12-17'))){
+build_training_datasets <- function(n_experiments = 5, samples = c(800, 100, 50, 20, 10, 5, 2), test_range = as.Date(c('2011-12-01', '2016-04-01'))){
 
   training <- buoy_data() %>%
-    filter(DateTime >= training_range[1], DateTime <= training_range[2])
+    filter(DateTime <= test_range[1] | DateTime >= test_range[2])
 
   un_dates <- training %>% pull(DateTime) %>% unique
 
   for (samp in samples){
     for (n in 1:n_experiments){
       n_exp <- stringr::str_pad(n, 2, pad = '0')
-      feather_filepath <- sprintf('../lake_modeling/data_imports/training_data_mendota_2009_2013/Mendota_training_2009_2013_%sprofiles_experiment_%s.feather', samp, n_exp)
-      figure_filepath <- sprintf('../lake_modeling/data_imports/figures/Mendota_training_2009_2013_%sprofiles_experiment_%s.png', samp, n_exp)
+      feather_filepath <- sprintf('../lake_modeling/data_imports/training_data_mendota/Mendota_training_%sprofiles_experiment_%s.feather', samp, n_exp)
+      figure_filepath <- sprintf('../lake_modeling/data_imports/figures/Mendota_training_%sprofiles_experiment_%s.png', samp, n_exp)
       set.seed(n)
       profile_dates <- sample(un_dates, samp, replace = FALSE)
       profiles <- training %>% filter(DateTime %in% profile_dates)
@@ -203,6 +207,16 @@ build_training_datasets <- function(n_experiments = 5, samples = c(800, 100, 50,
       feather::write_feather(profiles, feather_filepath)
     }
   }
+}
+
+build_test_dataset <- function(test_range = as.Date(c('2011-12-01', '2016-04-01'))){
+  feather_filepath <- '../lake_modeling/data_imports/test_data_mendota/Mendota_test.feather'
+  figure_filepath <- '../lake_modeling/data_imports/figures/Mendota_test.png'
+  test <- buoy_data() %>%
+    filter(DateTime > test_range[1] & DateTime < test_range[2])
+
+  subset_plots(figure_filepath, test, test_range)
+  feather::write_feather(test, feather_filepath)
 
 }
 
@@ -225,25 +239,111 @@ diagnostics <- function(){
 }
 
 
-subset_plots <- function(filepath, profiles){
+subset_plots <- function(filepath, profiles, xlim = as.Date(c('2009-04-01', '2017-12-17'))){
 
   temp_data <- profiles
-  xlim <- as.Date(c('2009-04-01', '2013-12-17'))
-  un_dates <- profiles %>% pull(DateTime) %>% unique
 
-  png(filename = filepath, width = 8, height = 3, units = 'in', res = 200)
+  un_dates <- temp_data %>% pull(DateTime) %>% unique
+
+  png(filename = filepath, width = 12, height = 3, units = 'in', res = 200)
   par(omi = c(0.4,0,0.05,0.05), mai = c(0,1,0,0))
   plot(xlim, c(NA,NA), xlim = xlim,
        ylim = c(24,0), xaxs = 'i', yaxs = 'i', ylab = 'Depth (m)', xlab = "")
 
   for (date in un_dates){
     data <- filter(temp_data, DateTime == date) %>% arrange()
-    col = colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan",
-                       "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))(30)
-    .filled.contour(x = c(date-0.5, date+0.5), y = data$Depth, z = rbind(data$temp,data$temp), levels = seq(0,to = 30), col = col)
+    if (nrow(data) > 2){
+      col = colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan",
+                               "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))(30)
+      .filled.contour(x = c(date-0.5, date+0.5), y = data$Depth, z = rbind(data$temp,data$temp), levels = seq(0,to = 30), col = col)
+
+    }
 
   }
   box()
   dev.off()
   #plot(buoy_temp$DateTime, buoy_temp$temp, xlim = as.Date(c('2009-04-10', '2013-11-01')), pch = 15, cex = 0.25)
 }
+
+train_test_overview_plots <- function(filepath, profiles, xlim = as.Date(c('2009-04-01', '2017-12-17'))){
+
+  filepath <- '~/Downloads/test_train_mendota_option2.png'
+
+  chunk_size <- 60
+  temp_data <- profiles
+
+  un_dates <- temp_data %>% pull(DateTime) %>% unique
+
+  png(filename = filepath, width = 14, height = 4, units = 'in', res = 200)
+  par(omi = c(0.25,0,0.05,0.05), mai = c(0.05,0.5,0,0), las = 1, mgp = c(2,.5,0))
+  layout(matrix(1:5))
+
+  plot(xlim, c(NA,NA), xlim = xlim,
+       ylim = c(24,0), xaxs = 'i', yaxs = 'i', ylab = 'Depth (m)', xlab = "", axes = FALSE)
+
+  axis(1, at = as.Date(c("2009-01-1","2010-01-01","2011-01-01","2012-01-01","2013-01-01","2014-01-01","2015-01-01","2016-01-01","2017-01-01")), labels = FALSE, tick = TRUE, tck = -0.02)
+  axis(2, at = seq(0,25,10), tck = -0.02)
+
+  un_dt_resample <- data.frame(date = un_dates, train = FALSE)
+  set.seed(42)
+  for (i in 1:10){
+    good_sample <- FALSE
+    while (!good_sample){
+      start_i <- sample(1:nrow(un_dt_resample), 1)
+      end_i <- start_i + chunk_size - 1
+      if (end_i <= nrow(un_dt_resample) & !any(un_dt_resample$train[start_i:end_i])){
+        good_sample = TRUE
+        un_dt_resample$train[start_i:end_i] <- TRUE
+        rect(xleft = un_dt_resample$date[start_i], ybottom = 25, xright = un_dt_resample$date[end_i], ytop = 0, col = '#0c0c0c47', border = NA)
+      } else {
+        message('moving on')
+      }
+    }
+  }
+
+  for (date in un_dates){
+    data <- filter(temp_data, DateTime == date) %>% arrange()
+    if (nrow(data) > 2){
+      col = colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan",
+                               "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))(30)
+      .filled.contour(x = c(date-0.5, date+0.5), y = data$Depth, z = rbind(data$temp,data$temp), levels = seq(0,to = 30), col = col)
+
+    }
+
+  }
+  text(as.Date("2009-04-10"), y = 4, "entire dataset", pos = 4)
+  box()
+
+  build_subset <- function(n){
+    plot(xlim, c(NA,NA), xlim = xlim,
+         ylim = c(24,0), xaxs = 'i', yaxs = 'i', ylab = 'Depth (m)', xlab = "", axes = FALSE)
+    axis(1, at = as.Date(c("2009-01-1","2010-01-01","2011-01-01","2012-01-01","2013-01-01","2014-01-01","2015-01-01","2016-01-01","2017-01-01")), labels = FALSE, tick = TRUE, tck = -0.02)
+    axis(2, at = seq(0,25,10), tck = -0.02)
+
+    profile_dates <- sample(un_dt_resample$date[!un_dt_resample$train], n, replace = FALSE)
+    for (date in profile_dates){
+      data <- filter(temp_data, DateTime == date) %>% arrange()
+      if (nrow(data) > 2){
+        col = colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan",
+                                 "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))(30)
+        .filled.contour(x = c(date-0.5, date+0.5), y = data$Depth, z = rbind(data$temp,data$temp), levels = seq(0,to = 30), col = col)
+
+      }
+
+    }
+    text(as.Date("2009-04-10"), y = 4, sprintf("training n=%s", n), pos = 4)
+    box()
+  }
+
+  build_subset(920)
+  build_subset(100)
+  build_subset(50)
+  build_subset(10)
+
+  axis(1, at = as.Date(c("2009-01-1","2010-01-01","2011-01-01","2012-01-01","2013-01-01","2014-01-01","2015-01-01","2016-01-01","2017-01-01")), labels = 2009:2017, tick = TRUE, tck = -0.02)
+
+  dev.off()
+
+}
+
+
