11d8865305e92b10e3aca3afbe3644c8009ebd43
Jordan S Read <jread@usgs.gov>
2019-02-26 15:57:12 -0600

    updated w/ buffered lake shapes

--- 2_crosswalk_munge/src/spatial_crosswalks.R
+++ 2_crosswalk_munge/src/spatial_crosswalks.R
@@ -30,3 +30,24 @@ combine_sf_lakes <- function(out_ind, ...){
   saveRDS(sf_lakes, data_file)
   gd_put(out_ind, data_file)
 }
+
+
+buffer_sf_lakes <- function(out_ind, lake_ind, buffer_width){
+  sf_lakes <- readRDS(scipiper::sc_retrieve(lake_ind)) %>%
+    st_transform(crs = "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs")
+
+  sf_buffered_lakes <- st_buffer(sf_lakes, dist = buffer_width)
+
+  sf_donut_lakes <- sf_lakes
+  for (j in 1:nrow(sf_donut_lakes)){
+    sf_donut_lakes[j, ] <- st_difference(sf_buffered_lakes[j, ], sf_lakes[j, ]) %>% select(site_id, geometry)
+  }
+  sf_donut_lakes <- sf_donut_lakes %>%
+    select(site_id, geometry) %>%
+    st_transform(crs = 4326)
+
+  data_file <- scipiper::as_data_file(out_ind)
+  saveRDS(sf_donut_lakes, data_file)
+  gd_put(out_ind, data_file)
+
+}
