6932a852f406feb4518921020c7cca9817d69804
Jordan S Read <jread@usgs.gov>
2019-02-26 09:08:53 -0600

    moving things around and rebuilding targets

--- 1_crosswalk_fetch/src/munge_crosswalk.R
+++ 1_crosswalk_fetch/src/munge_crosswalk.R
@@ -4,9 +4,35 @@
 #'   making up the shapefile
 munge_crosswalk <- function(out_ind, shp_ind, ...) {
   # read the file
-  shp <- sf::st_read(scipiper::sc_retrieve(shp_ind))
+  shp <- sf::st_read(scipiper::sc_retrieve(shp_ind)) %>% select(site_id)
 
   # munging could happen here
+
+  # write, post, and promise the file is posted
+  data_file <- scipiper::as_data_file(out_ind)
+  saveRDS(shp, data_file)
+  gd_put(out_ind, data_file)
+}
+
+
+#' we take the MGLP geodatabase and read it into sf,
+#' then remove lakes that aren't part of the assessment and/or aren't
+#' part of the expansion footprint, as defined by state IDs in ...
+#'
+#' @param ... state IDs to include (e.g., "SD" for South Dakota)
+#'
+MGLP_zip_to_sf <- function(out_ind, gdb_file, zip_ind, ...){
+  states <- c(...)
+  zip_file <- scipiper::sc_retrieve(zip_ind)
+
+  shp.path <- tempdir()
+  unzip(zip_file, exdir = shp.path)
+
+  shp <- sf::st_read(file.path(shp.path, gdb_file), layer = 'MGLP_LAKES') %>%
+    filter(ASSESS == 'Y', STATE %in% states) %>%
+    mutate(site_id = paste0('mglp_', LAKE_ID)) %>% select(site_id, geometry = SHAPE) %>% # why do I need to rename SHAPE to geometry??
+    st_transform(x, crs = 4326)
+
 
   # write, post, and promise the file is posted
   data_file <- scipiper::as_data_file(out_ind)
--- 2_crosswalk_munge/src/spatial_crosswalks.R
+++ 2_crosswalk_munge/src/spatial_crosswalks.R
@@ -17,3 +17,16 @@ crosswalk_points_in_poly <- function(ind_file, poly_ind_file, points_ind_file){
   saveRDS(crosswalked_ids, data_file)
   gd_put(ind_file, data_file)
 }
+
+
+combine_sf_lakes <- function(out_ind, ...){
+  sf_inds <- c(...)
+
+  sf_lakes <- readRDS(scipiper::sc_retrieve(sf_inds[1L]))
+  for (sf_ind in tail(sf_inds, -1)){
+    sf_lakes <- rbind(sf_lakes, readRDS(scipiper::sc_retrieve(sf_ind)))
+  }
+  data_file <- scipiper::as_data_file(out_ind)
+  saveRDS(sf_lakes, data_file)
+  gd_put(out_ind, data_file)
+}
