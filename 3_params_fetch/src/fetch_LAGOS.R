cb78938612e1a5b77cca5d62eb18d15a94ca5d2e
Jordan S Read <jread@usgs.gov>
2019-02-28 20:07:10 -0600

    adding secchi

--- 3_params_fetch/src/fetch_LAGOS.R
+++ 3_params_fetch/src/fetch_LAGOS.R
@@ -8,3 +8,12 @@ fetch_LAGOS_NE_depths <- function(out_ind){
 
   gd_put(out_ind, data_file)
 }
+
+fetch_LAGOS_NE_secchi <- function(out_ind){
+
+  data_file <- scipiper::as_data_file(out_ind)
+  download.file(url = "https://portal.edirepository.org/nis/dataviewer?packageid=edi.101.2&entityid=f02dd4d3375bed6871a33c648ef1631e",
+                destfile = data_file)
+
+  gd_put(out_ind, data_file)
+}
