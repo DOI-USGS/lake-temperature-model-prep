b8d7a48d8d59ada4eab303789d3f54e01a96aa8b
Jordan S Read <jread@usgs.gov>
2019-08-05 09:43:01 -0500

    new param fetch/munge builds

--- 4_params_munge/src/munge_lagos.R
+++ 4_params_munge/src/munge_lagos.R
@@ -1,7 +1,8 @@
 
+#' format for these outputs: site_id, {source}_ID, mon_group, date, secchi
+munge_lagos_secchi <- function(out_ind, lagos_secchi_ind, lagos_xwalk_ind){
 
-munge_lagos_secchi <- function(out_ind, lagos_secchi_ind){
-
+  lagos_xwalk <- sc_retrieve(lagos_xwalk_ind) %>% readRDS()
   lagos_secchi <- sc_retrieve(lagos_secchi_ind) %>%
     read_csv(col_types = cols(
       eventidc1087 = col_double(),
@@ -16,11 +17,40 @@ munge_lagos_secchi <- function(out_ind, lagos_secchi_ind){
       secchi_methodinfo = col_character(),
       sampleyear = col_double(),
       samplemonth = col_double())) %>%
-    mutate(site_id = sprintf("lagos_%s", lagoslakeid)) %>%
-    dplyr::select(site_id, date = sampledate, secchi, mon_group = programname)
+    mutate(LAGOS_ID = sprintf("lagos_%s", lagoslakeid)) %>%
+    dplyr::inner_join(lagos_xwalk, by = 'LAGOS_ID') %>%
+    dplyr::select(site_id, LAGOS_ID, mon_group = programname, date = sampledate, secchi)
 
   data_file <- scipiper::as_data_file(out_ind)
   saveRDS(lagos_secchi, data_file)
+  gd_put(out_ind, data_file)
+
+}
+
+
+#' format for these outputs: site_id, {source}_ID, z_max_source, z_max
+munge_lagos_depths <- function(out_ind, lagos_depths_ind, lagos_xwalk_ind){
+
+  lagos_xwalk <- sc_retrieve(lagos_xwalk_ind) %>% readRDS()
+
+  lagos_depths <- sc_retrieve(lagos_depths_ind) %>%
+    read_csv(col_types = cols(
+      lagoslakeid = col_double(),
+      nhdid = col_character(),
+      nhd_lat = col_double(),
+      nhd_long  = col_double(),
+      lagosname1 = col_character(),
+      meandepth = col_double(),
+      meandepthsource = col_character(),
+      zmaxobs = col_double(),
+      maxdepthsource = col_character(),
+      legacyid = col_character())) %>%
+    mutate(LAGOS_ID = sprintf("lagos_%s", lagoslakeid)) %>%
+    dplyr::inner_join(lagos_xwalk, by = 'LAGOS_ID') %>%
+    dplyr::select(site_id, LAGOS_ID, z_max_source = maxdepthsource, z_max = zmaxobs)
+
+  data_file <- scipiper::as_data_file(out_ind)
+  saveRDS(lagos_depths, data_file)
   gd_put(out_ind, data_file)
 
 }
--- 4_params_munge/src/munge_mglp.R
+++ 4_params_munge/src/munge_mglp.R
@@ -1,11 +1,11 @@
 
+#' format for these outputs: site_id, {source}_ID, z_max
+mglp_depths_from_TDOx <- function(out_ind, TDOx_ind, mglp_xwalk_ind){
 
-mglp_depths_from_TDOx <- function(out_ind, TDOx_ind){
 
-  TDOx_data <- sc_retrieve(TDOx_ind) %>% read_csv
-
-  mglp_depths <- TDOx_data %>% mutate(site_id = sprintf("mglp_%s", mglp_id)) %>%
-    dplyr::select(site_id, z_max = max_depth)
+  mglp_xwalk <- sc_retrieve(mglp_xwalk_ind) %>% readRDS()
+  mglp_depths <- sc_retrieve(TDOx_ind) %>% read_csv() %>% mutate(MGLP_ID = sprintf("MGLP_%s", mglp_id)) %>%
+    inner_join(mglp_xwalk, by = 'MGLP_ID') %>% dplyr::select(site_id, MGLP_ID, z_max = max_depth)
 
   data_file <- scipiper::as_data_file(out_ind)
   saveRDS(mglp_depths, data_file)
