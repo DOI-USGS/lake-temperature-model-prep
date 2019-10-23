569c6819e59d03066fd534110e46cf170e1760e5
Jordan S Read <jread@usgs.gov>
2019-10-23 17:20:53 -0500

    dealing w/ multiple bathys and zip.ind download

--- 4_params_munge/src/munge_bathy.R
+++ 4_params_munge/src/munge_bathy.R
@@ -15,51 +15,39 @@ munge_wbic_bathy <- function(out_ind, bathy_zip_ind, wbic_xwalk_ind){
   gd_put(out_ind, data_file)
 }
 
-munge_mndow_bathy <- function(out_ind, bathy_list_ind, mndow_xwalk_ind){
-
-  bathy_list <- readRDS(sc_retrieve(bathy_list_ind))
+munge_mndow_bathy <- function(out_ind, bathy_zip_ind, mndow_xwalk_ind){
 
   mndow_xwalk <- readRDS(sc_retrieve(mndow_xwalk_ind))
 
-  bathy_dat <- NULL
-
-  # Loop through files and bind together
-  for (i in 1:nrow(bathy_list)){
-    csv_name <- bathy_list$name[i]
-    csv_id <- as_id(bathy_list$id[i])
-
-    lake_dow <- unlist(strsplit(csv_name, ".csv"))
-    lake_dow <- unlist(strsplit(lake_dow, "_"))
-    lake_dow <- lake_dow[length(lake_dow)]
+  bth_dir <- tempdir()
 
 
-    # Create folder to save downloaded files
-    dir.create("3_params_fetch/in/hypsos_m/", showWarnings = FALSE)
-    # Download csv's
+  # this still has the state-specific ID, since some NHDHR ids (site_id) have more than one. We need to combine those later
+  bathy_data_mapped <- sc_retrieve(bathy_zip_ind) %>% unzip(exdir = bth_dir) %>%
+    purrr::map(function(x) {
+      MNDOW_ID <- basename(x) %>% str_extract('_[0-9]+.csv$') %>% str_extract('[0-9]+') %>% paste0('mndow_', .)
+      read_csv(x, col_types = 'dd') %>% mutate(MNDOW_ID = MNDOW_ID)
+    }) %>% purrr::reduce(rbind) %>%
+    inner_join(mndow_xwalk, by = 'MNDOW_ID')
+  browser()
+  # now time to combine
+  bathy_data <- purrr::map(bathy_data_mapped$site_id, function(x){
 
-    if (!file.exists(paste0("3_params_fetch/in/hypsos_m/",csv_name))){
-      drive_download(csv_id, path=paste0("3_params_fetch/in/hypsos_m/",csv_name), overwrite = TRUE)
-      message(paste0("Downloading ", csv_name))
+    this_bathy <- filter(bathy_data_mapped, site_id == x)
+    state_ids <- unique(this_bathy$MNDOW_ID)
+    if (length(state_ids) > 1){
+      z_all <- round(this_bathy$depths, 1) %>% unique() %>% sort
+      purrr::map(state_ids, function(id){
+        ._d <- filter(this_bathy, MNDOW_ID == id)
+        data.frame(site_id = x,
+                   areas = approx(xout = z_all, x = ._d$depths, y = ._d$areas, rule = 2:1)$y, depths = z_all, stringsAsFactors = FALSE)
+      }) %>% reduce(rbind) %>%
+        group_by(depths, site_id) %>% summarize(areas = sum(areas, na.rm = TRUE))
+    } else {
+      this_bathy
     }
 
-    #gd_confirm_posted(as_ind_file(paste0("3_params_fetch/in/hypsos_m/",csv_name)))
-    #gd_get(as_ind_file(paste0("3_params_fetch/in/hypsos_m/",csv_name)))
-
-
-    # Read csv's
-    message(paste0("Reading ", csv_name))
-
-    lake_hypso <- read.csv(paste0("3_params_fetch/in/hypsos_m/",csv_name), header=T)
-    # Add lake DOW
-    lake_hypso$MNDOW_ID <- paste0('mndow_', lake_dow)
-
-    # Bind lakes together - there is a slicker way to do this
-    bathy_dat <- rbind(bathy_dat, lake_hypso)
-
-  }
-
-  bathy_dat <- bathy_dat %>% left_join(mndow_xwalk, by = 'MNDOW_ID') %>%
-    dplyr::select(site_id, depths, areas)
+  }) %>% purrr::reduce(rbind) %>% dplyr::select(site_id, depths, areas)
 
 
   data_file <- scipiper::as_data_file(out_ind)
