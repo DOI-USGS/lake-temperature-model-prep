2d4a3c0a46dd0cf779166e63528cc56a889c22e6
Platt <lplatt@usgs.gov>
2020-02-10 12:24:41 -0600

    address issues with zero area shapes + filter out problem lakes for now

--- 4_params_munge/src/munge_bathy.R
+++ 4_params_munge/src/munge_bathy.R
@@ -22,7 +22,6 @@ collapse_multi_bathy <- function(data_in){
     these_bathys <- filter(data_in, site_id == x)
     state_ids <- unique(these_bathys$other_ID)
 
-
     if (length(state_ids) > 1){ # will need to combine multiples:
 
       z_all <- round(these_bathys$depths, 1) %>% unique() %>% sort
@@ -103,34 +102,77 @@ munge_mndow_bathy <- function(out_ind, bathy_zip_ind, mndow_xwalk_ind){
   gd_put(out_ind, data_file)
 }
 
-munge_ndgf_bathy <- function(out_ind, ndgf_contour_ind, ndgf_xwalk_ind){
+munge_ndgf_bathy <- function(out_ind, ndgf_contour_ind, ndgf_xwalk_ind, ndgf_surface_sf_ind, crs){
 
   ndgf_xwalk <- readRDS(sc_retrieve(ndgf_xwalk_ind))
 
-  # Use the lake surface polygon to join
+  # Now crosswalk with NHD lake ids
   ndgf_contours <- readRDS(sc_retrieve(ndgf_contour_ind)) %>%
     mutate(NDGF_ID = sprintf("ndgf_%s", LAKE)) %>%
     inner_join(ndgf_xwalk, by = 'NDGF_ID') %>%
     rename(other_ID = NDGF_ID,
-           depths = CONTOUR)
+           depths = CONTOUR) %>%
+    dplyr::select(site_id, other_ID, depths)
 
-  # The polygon version causes an error if you use it with dplyr functions after st_polygonize
-  # E.g. `found multiple dimensions: XYZ XY`
-  ndgf_contours_polygons <- st_polygonize(ndgf_contours)
+  # Due to some issues with incomplete surface polygons in the contour data
+  #   for some lakes causing zero area lakes and ultimately monotonicity issues,
+  #   replace all contour shapes (depth = 0) with the polygons from ndgf_lakesvw.
 
-  # `st_area` doesn't return the expected result when added with mutate()
-  # E.g. `mutate(areas = as.numeric(st_area(geometry)))`
-  ndgf_areas <- st_area(ndgf_contours_polygons)
+  # We will treat these separately until after issues with some of the contour data are resolved
 
-  # Now add to contour geom as a column and reformat
-  ndgf_bathy <- ndgf_contours %>%
-    mutate(areas = as.numeric(ndgf_areas)) %>%
-    dplyr::select(site_id, other_ID, depths, areas)
+  # Many of the lakes have multiple polygons at the same depth. Some of the smaller
+  # polygons at the same depth are contained within a larger polygon at the same depth.
+  # This doesn't make much sense and we end up double counting the area. So, union them
+  # first to reconcile internal boundaries by lake and then find the area.
 
-  # I'm not sure if I need to use `collapse_multi_bathy` or not
-  # All other function examples use it
+  ndgf_non_surface_geoms <- ndgf_contours %>%
+    filter(depths != 0) %>%
+    group_by(site_id, other_ID, depths) %>%
+    summarize(geometry = st_union(geometry)) %>%
+    mutate(areas = as.numeric(st_area(st_polygonize(geometry)))) %>%
+    dplyr::select(site_id, other_ID, depths, areas) %>%
+    ungroup() %>%
+    st_transform(crs = crs)
+
+  # Now isolate the surface geometries for the lakes in the contour dataset
+  # Make sure to join the crosswalk
+  ndgf_surface_geoms <- readRDS(sc_retrieve(ndgf_surface_sf_ind)) %>%
+    rename(NDGF_ID = site_id) %>%
+    left_join(ndgf_xwalk) %>%
+    filter(NDGF_ID %in% unique(ndgf_non_surface_geoms$other_ID)) %>%
+    mutate(depths = 0, areas = as.numeric(st_area(geometry))) %>%
+    # Needs to be in the same order as non surface one because we are binding rows
+    select(site_id, other_ID = NDGF_ID, depths, areas) %>%
+    # Convert geometry type so it matches the type coming from contour data
+    st_cast("MULTILINESTRING") %>%
+    st_transform(crs = crs)
+
+  # Combine the surface only geoms with the non surface geoms
+  #   assumes that they have the same columns in the same order
+  ndgf_geoms <- rbind(ndgf_surface_geoms, ndgf_non_surface_geoms)
+
+  # Then, format to get ready for next step
+  ndgf_bathy <- ndgf_geoms %>%
+    filter(!other_ID %in% "ndgf_516") %>% # This lake has some weirdness ... has both positive (not just 0) and negative depths
+    # Bathy needs to be positive depth (assumes depth = "feet below surface") and
+    #   starts from surface moving down (so arrange by increasing depth)
+    mutate(depths = -depths) %>%
+    arrange(depths)
+
+  not_working<-c("752", "751", "753", "750", "077", "020","046",
+                 "001", "551", "085", "305","646", "285")
+
+  ndgf_bathy_valid <- ndgf_bathy %>%
+    # There are still shapes with zero areas (and no other source of
+    # shape information like we had with the surface depths), so we
+    # filter them out so that they do not cause issues
+    filter(areas != 0) %>%
+    # IDs stored in `not_working` are not passing monotonicity
+    # Still investigating why that is the issue.
+    filter(other_ID %in% sprintf("ndgf_%s", not_working))
+
   data_file <- scipiper::as_data_file(out_ind)
-  collapse_multi_bathy(ndgf_bathy) %>%
+  collapse_multi_bathy(ndgf_bathy_valid) %>%
     saveRDS(data_file)
   gd_put(out_ind, data_file)
 }
