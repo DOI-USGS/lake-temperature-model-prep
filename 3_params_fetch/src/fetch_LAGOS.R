04d7db952cf73cd84b6889a9284285617791eb68
Jordan S Read <jread@usgs.gov>
2020-10-08 11:40:30 -0500

    use httr::GET here instead

--- 3_params_fetch/src/fetch_LAGOS.R
+++ 3_params_fetch/src/fetch_LAGOS.R
@@ -3,8 +3,8 @@
 fetch_LAGOS_NE_depths <- function(out_ind){
 
   data_file <- scipiper::as_data_file(out_ind)
-  download.file(url = "https://portal.edirepository.org/nis/dataviewer?packageid=edi.101.3&entityid=df2f94197ed33bc6f3052511b23a721e",
-                destfile = data_file)
+  httr::GET(url = "https://portal.edirepository.org/nis/dataviewer?packageid=edi.101.3&entityid=df2f94197ed33bc6f3052511b23a721e",
+            write_disk(data_file, overwrite=TRUE))
 
   read_csv(data_file) %>% filter(!is.na(maxdepth)) %>%
     rename(zmaxobs = maxdepth) %>% arrange(lagoslakeid) %>% write_csv(path = data_file)
@@ -15,7 +15,7 @@ fetch_LAGOS_NE_depths <- function(out_ind){
 fetch_LAGOS_NE_secchi <- function(out_ind){
 
   data_file <- scipiper::as_data_file(out_ind)
-  download.file(url = "https://portal.edirepository.org/nis/dataviewer?packageid=edi.101.2&entityid=f02dd4d3375bed6871a33c648ef1631e",
-                destfile = data_file)
+  httr::GET(url = "https://portal.edirepository.org/nis/dataviewer?packageid=edi.101.2&entityid=f02dd4d3375bed6871a33c648ef1631e",
+            write_disk(data_file, overwrite=TRUE))
   gd_put(out_ind, data_file)
 }
