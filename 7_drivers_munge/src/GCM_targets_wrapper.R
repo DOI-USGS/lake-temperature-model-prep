ee543596a6faa202be93f15714a9a81dbdbb751c
Corson-Dosch <hcorson-dosch@usgs.gov>
2021-11-10 17:16:22 -0600

    Add some comments

--- 7_drivers_munge/src/GCM_targets_wrapper.R
+++ 7_drivers_munge/src/GCM_targets_wrapper.R
@@ -1,7 +1,10 @@
 build_GCM_pipeline <- function(ind_file, ...) {
-
+  # Build the targets pipeline
   tar_make()
 
+  # Load the final target that lists the output files
   tar_load(pipeline_files_out)
+
+  # Indicate those file names and hashes
   sc_indicate(ind_file, data_file=pipeline_files_out)
 }
