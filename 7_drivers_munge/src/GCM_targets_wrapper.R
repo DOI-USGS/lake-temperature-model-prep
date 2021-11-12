2c8cf10263c7895aea6908f3968b4ae8e5af8a4d
Corson-Dosch <hcorson-dosch@usgs.gov>
2021-11-12 09:27:28 -0600

    In wrapper, build final file summary target, rather than whole targets pipeline

--- 7_drivers_munge/src/GCM_targets_wrapper.R
+++ 7_drivers_munge/src/GCM_targets_wrapper.R
@@ -1,6 +1,6 @@
 build_GCM_pipeline <- function(ind_file, ...) {
   # Build the targets pipeline
-  tar_make()
+  tar_make(pipeline_files_out)
 
   # Load the final target that lists the output files
   tar_load(pipeline_files_out)
