ac16891791e5a99b1732cbc6ae563aa425c9856d
Platt <lplatt@usgs.gov>
2021-11-12 12:13:53 -0600

    edit pipeline to do very basic GCM download of GFDL for 5 WI lakes

--- 7_drivers_munge/src/GCM_driver_utils.R
+++ 7_drivers_munge/src/GCM_driver_utils.R
@@ -8,3 +8,51 @@ map_centroids <- function(centroids_sf_rds, out_file) {
 
   return(out_file)
 }
+
+# TODO: a much better solution. For now, this just subsets the lakes
+# to 5 in WI, but it will need to do smart chunking at some point.
+split_lake_centroids <- function(centroids_sf_rds) {
+
+  set.seed(19) # Same subset of 5 every time
+  wi_sf <- st_as_sf(maps::map('state', 'wisconsin', plot=FALSE, fill=TRUE))
+
+  readRDS(centroids_sf_rds) %>%
+    st_intersection(wi_sf) %>%
+    sample_n(5)
+}
+
+# Take the lake centroids and convert into a polygon to query
+# the Geo Data Portal for driver data.
+centroids_to_poly <- function(centroids_sf) {
+  centroids_sf %>%
+    st_make_grid()
+}
+
+# Convert an sf object into a geoknife::simplegeom, so that
+# it can be used in the geoknife query. Must become an
+# `sp` object first.
+sf_to_simplegeom <- function(sf_obj) {
+  sf_obj %>%
+    as_Spatial() %>%
+    simplegeom()
+}
+
+# Set up a download file to get the raw GCM data down.
+# This function accepts each of the query parameters for the
+# geoknife job as an argument. Currently missing the "knife" parameter.
+download_gcm_data <- function(out_file, query_geom, query_url, query_vars,
+                              query_dates, query_knife = NULL) {
+  gcm_job <- geoknife(
+    stencil = query_geom,
+    fabric = webdata(
+      url = query_url,
+      variables = query_vars,
+      times = query_dates
+    )
+  )
+
+  wait(gcm_job)
+  download(gcm_job, destination = out_file, overwrite = TRUE)
+
+  return(out_file)
+}
