73b4f2d034fb506efd448ef664282cf7c920cdec
Corson-Dosch <hcorson-dosch@usgs.gov>
2021-11-30 16:13:08 -0600

    Clean up targets, branch over sf obj, set up query to cross branch, move simplegeom conversion into query

--- 7_drivers_munge/src/GCM_driver_utils.R
+++ 7_drivers_munge/src/GCM_driver_utils.R
@@ -1,19 +1,3 @@
-
-map_query <- function(out_file, centroids_sf) {
-
-  # TODO: delete this WI-specific view
-  wi_sf <- st_as_sf(maps::map('state', 'wisconsin', plot=FALSE, fill=TRUE))
-
-  query_plot <- ggplot() +
-    geom_sf(data=wi_sf) +
-    geom_sf(data=centroids_sf, color='salmon', size=4) +
-    coord_sf() + theme_void()
-
-  ggsave(out_file, query_plot, width=10, height=8, dpi=300)
-
-  return(out_file)
-}
-
 # TODO: a much better solution. For now, this just subsets the lakes
 # to 5 in WI, but it will need to do smart chunking at some point.
 split_lake_centroids <- function(centroids_sf_rds) {
@@ -26,33 +10,38 @@ split_lake_centroids <- function(centroids_sf_rds) {
     sample_n(5)
 }
 
-# Reproject to the crs of the gcm grid
-project_to_grid_crs <- function(input_sf, grid_cells) {
-  sf::st_transform(input_sf, crs = st_crs(grid_cells))
+# Reproject to the specified crs
+project_to_crs <- function(sf_obj, crs) {
+  sf::st_transform(sf_obj, crs = st_crs(crs))
 }
 
+# Read in shp as sf and project to grid crs
+read_and_project_shp <- function(shp, grid_crs) {
+  sf_obj <- sf::st_read(shp)
+  sf_obj_prj <- project_to_crs(sf_obj, grid_crs)
+  return(sf_obj_prj)
+}
 
-# need to subset grid tiles polygons to that with mapped tile id
-# then subset grid cells to polygons within that tile
-# using centroids for clean intersection
-# then filtering cell polygons to those w/ subsetted ids
-get_tile_cells <- function(grid_cell_centroids, grid_cells_sf, grid_tiles, grid_tile_id) {
-  grid_tile <- grid_tiles %>%
-    filter(tile_no == grid_tile_id)
-
+# Subset grid cells to those within each tile
+# (targets is branching over the tile polygons)
+# using grid cell centroids for clean intersection
+get_tile_cells <- function(grid_cell_centroids, grid_cells, grid_tile) {
+  # Figure out which cell centroids fall within each tile
   tile_cells <- grid_cell_centroids %>%
     st_intersection(grid_tile)
 
+  # Pull the cell ids associated with each tile
   tile_cell_ids <- tile_cells$cell_no
 
-  grid_cells_sf %>%
+  # Filter grid cells to those with the specified ids
+  grid_cells %>%
     filter(cell_no %in% tile_cell_ids) %>%
-    mutate(tile_no = grid_tile_id)
+    mutate(tile_no = grid_tile$tile_no)
 }
 
 # Filter cells associated with given tile
 # to only those cells that contain lakes
-keep_cells_with_lakes <- function(tile_cells, lake_centroids) {
+get_query_cells <- function(tile_cells, lake_centroids) {
   cell_lakes_intersect <- st_intersects(tile_cells, lake_centroids)
 
   cells_w_lakes <- tile_cells %>% mutate(contains_lake = lengths(cell_lakes_intersect) > 0, n_lakes = lengths(cell_lakes_intersect)) %>%
@@ -62,19 +51,21 @@ keep_cells_with_lakes <- function(tile_cells, lake_centroids) {
 
 }
 
-
+# Filter cell centroids to those that fall within
+# the grid cells that contain lakes (for a given tile)
 get_query_centroids <- function(query_cells, cell_centroids) {
   # remove cell_no column from cell_centroids, since don't need it
   # and would be duplicated on join
   cell_centroids <- cell_centroids %>% select(-cell_no)
 
+  # keep only cell centroids for cells with lakes
   cell_centroids_w_lakes <- cell_centroids %>%
     st_join(query_cells, left=FALSE)
 
   return(cell_centroids_w_lakes)
 }
 
-# Get lake cell xwalk
+# Get lake cell xwalk, w/o spatial information
 get_lake_cell_xwalk <- function(lake_centroids, tile_cells) {
   lake_cells_join <- lake_centroids %>%
     st_join(tile_cells, left=FALSE) %>%
@@ -83,42 +74,106 @@ get_lake_cell_xwalk <- function(lake_centroids, tile_cells) {
   return(lake_cells_join)
 }
 
+# Map the query - grid cells, grid tiles, selected tile, cells w lakes in selected tile, lake centroids
+# For now, generating empty file for tiles that don't contain cells that contain lakes
+map_query <- function(out_file_template, lake_centroids, grid_tiles, grid_cells, cells_w_lakes) {
+  if (nrow(cells_w_lakes) > 0) {
+    # get tile id (pull from first row - will be identical
+    # for all rows, since branching over tiles)
+    tile_id <- cells_w_lakes$tile_no[1]
+
+    # get tile polygon for selected tile
+    selected_tile <- grid_tiles %>% filter(tile_no == tile_id)
+
+    # TODO: delete this WI-specific view
+    wi_sf <- st_as_sf(maps::map('state', 'wisconsin', plot=FALSE, fill=TRUE)) %>%
+      sf::st_transform(crs = sf::st_crs(grid_cells))
+
+    # build plot
+    query_plot <- ggplot() +
+      geom_sf(data=wi_sf, fill=NA, color='grey70') +
+      geom_sf(data=grid_cells, color='grey50', fill=NA, size=0.5) +
+      geom_sf(data=grid_tiles, color='dodgerblue3', fill=NA) +
+      geom_sf(data=cells_w_lakes, color='firebrick3', fill=NA) +
+      geom_sf(data=selected_tile, color='darkturquoise', fill=NA) +
+      geom_sf(data=lake_centroids, color='black', size=0.5) +
+      coord_sf() + theme_void()
+
+    # save file
+    out_file <- gsub('XX', tile_id, out_file_template)
+    ggsave(out_file, query_plot, width=10, height=8, dpi=300)
+
+    return(out_file)
+  } else {
+    # MUST BE A BETTER WORKAROUND?
+    # Build placeholder out_file name
+    out_file <- gsub('XX', 'NULL', out_file_template)
+
+    # write empty file
+    query_plot_empty <- ggplot()
+    ggsave(out_file, query_plot_empty)
+
+    return(out_file)
+  }
+}
 
 # Convert an sf object into a geoknife::simplegeom, so that
 # it can be used in the geoknife query. `geoknife` only works
 # with `sp` objects but not SpatialPoints at the moment, so
 # need to convert these to a data.frame.
-#
-# Added an if statement to catch dataframes w/ 0 rows
-# since using this to map over tiles that may not
-# have any cells that contain lakes, and therefore
-# no cell centroids to convertto simplegeom
 sf_pts_to_simplegeom <- function(sf_obj) {
-  if (nrow(sf_obj) > 0 ){
     sf_obj %>%
       st_coordinates() %>%
       t() %>% as.data.frame() %>%
       simplegeom()
-  }
 }
 
 # Set up a download file to get the raw GCM data down.
 # This function accepts each of the query parameters for the
 # geoknife job as an argument. Currently missing the "knife" parameter.
-download_gcm_data <- function(out_file, query_geom, query_url, query_vars,
+download_gcm_data <- function(out_file_template, query_geom, query_url_template, gcm_name, query_vars,
                               query_dates, query_knife = NULL) {
-  gcm_job <- geoknife(
-    stencil = query_geom,
-    fabric = webdata(
-      url = query_url,
-      variables = query_vars,
-      times = query_dates
+  #IF TILE CONTAINS CELLS THAT CONTAIN LAKES
+  if (nrow(query_geom) > 0) {
+
+    # Reproject query cell centroids to WGS84
+    query_geom_WGS84 <- project_to_crs(query_geom, crs = 4326)
+
+    # convert grid cell centroids into geoknife-friendly format
+    query_simplegeom <- sf_pts_to_simplegeom(query_geom_WGS84)
+
+    # Build query_url
+    query_url <- gsub('GCMname', gcm_name, query_url_template)
+
+    # construct and submit query
+    gcm_job <- geoknife(
+      stencil = query_simplegeom,
+      fabric = webdata(
+        url = query_url,
+        variables = query_vars,
+        times = query_dates
+      )
     )
-  )
+    wait(gcm_job)
+    my_data <- result(gcm_job)
 
-  wait(gcm_job)
-  my_data <- result(gcm_job)
-  arrow::write_feather(my_data, out_file)
+    # Build out_file name
+    out_file <- gsub('GCMname', gcm_name, out_file_template)
+    out_file <- gsub('XX', query_geom$tile_no[1], out_file)
 
-  return(out_file)
+    # write file
+    arrow::write_feather(my_data, out_file)
+
+    return(out_file)
+  } else {
+    # MUST BE A BETTER WORKAROUND?
+    # Build placeholder out_file name
+    out_file <- gsub('GCMname', 'NULL', out_file_template)
+    out_file <- gsub('XX', 'NULL', out_file)
+
+    # write empty data frame
+    arrow::write_feather(data.frame(), out_file)
+
+    return(out_file)
+  }
 }
--- _targets.R
+++ _targets.R
@@ -15,163 +15,129 @@ source('7_drivers_munge/src/GCM_driver_utils.R')
 
 targets_list <- list(
   # The input from our `scipiper` pipeline
-  tar_target(
-    lake_centroids_sf_rds,
-    gd_get('2_crosswalk_munge/out/centroid_lakes_sf.rds.ind'),
-    format='file'
-  ),
+  tar_target(lake_centroids_sf_rds, gd_get('2_crosswalk_munge/out/centroid_lakes_sf.rds.ind'), format='file'),
 
   # FOR NOW, FOR TESTING - get subset of lake centroids
-  tar_target(
-    subset_lake_centroids_sf,
-    split_lake_centroids(lake_centroids_sf_rds)
-  ),
+  tar_target(subset_lake_centroids_sf, split_lake_centroids(lake_centroids_sf_rds)),
 
-  # TODO: bring in the python workflow to create this shapefile
-  # of the reconstructed grid cells
+  # TODO: bring in the python workflow to create this shapefile of the reconstructed grid cells
   # Load in the reconstructed grid shapefile as a file target
-  tar_target(
-    grid_cells_shp,
-    '7_drivers_munge/in/gcm_grid_cells.shp',
-    format='file'
-  ),
+  tar_target(grid_cells_shp, '7_drivers_munge/in/gcm_grid_cells.shp', format='file'),
 
   # load the grid cell shapefile using readOGR
   # (so that we get the custom LCC projection information)
-  tar_target(
-    grid_cells,
-    rgdal::readOGR(grid_cells_shp)
-  ),
+  tar_target(grid_cells_ogr, rgdal::readOGR(grid_cells_shp)),
 
   # Load grid cells shapefile again as sf object
   # using projection of grid cells OGR object
-  tar_target(
-    grid_cells_sf,
-    sf::st_read(grid_cells_shp, layer='gcm_grid_cells') %>%
-      sf::st_transform(grid_cells, crs = sf::st_crs(grid_cells))
-  ),
+  tar_target(grid_cells_sf, read_and_project_shp(grid_cells_shp, grid_cells_ogr)),
 
-  # TODO: bring in the python workflow to create this shapefile
-  # of the grid cell centroids
+  # TODO: bring in the python workflow to create this shapefile of the grid cell centroids
   # Load in the grid cell centroids as a file target
-  tar_target(
-    grid_cell_centroids_shp,
-    '7_drivers_munge/in/gcm_grid_cell_centroids.shp',
-    format='file'
-  ),
+  tar_target(grid_cell_centroids_shp, '7_drivers_munge/in/gcm_grid_cell_centroids.shp', format='file'),
 
-  # load the tiles shapefile using sf
+  # Load the grid cell centroids shapefile using sf
   # using projection of grid cells OGR object
-  tar_target(
-    grid_cell_centroids_sf,
-    sf::st_read(grid_cell_centroids_shp, layer='gcm_grid_cell_centroids') %>%
-      sf::st_transform(grid_cells, crs = sf::st_crs(grid_cells))
-  ),
+  tar_target(grid_cell_centroids_sf, read_and_project_shp(grid_cell_centroids_shp, grid_cells_ogr)),
 
-  # TODO: bring in the python workflow to create this shapefile
-  # of the tiles
+  # TODO: bring in the python workflow to create this shapefile of the tiles
   # Load in the shapefile of the grid tiles as a file target
-  tar_target(
-    grid_tiles_shp,
-    '7_drivers_munge/in/gcm_grid_tiles.shp',
-    format='file'
-  ),
+  tar_target(grid_tiles_shp, '7_drivers_munge/in/gcm_grid_tiles.shp', format='file'),
 
-  # load the tiles shapefile using sf
+  # Load the grid tiles shapefile using sf
   # using projection of grid cells OGR object
-  tar_target(
-    grid_tiles_sf,
-    sf::st_read(grid_tiles_shp, layer='gcm_grid_tiles') %>%
-      sf::st_transform(grid_cells, crs = sf::st_crs(grid_cells))
-  ),
+  tar_target(grid_tiles_sf,read_and_project_shp(grid_tiles_shp, grid_cells_ogr)),
 
-  # reproject lake centroids to crs of grid cells
-  tar_target(
-    query_lake_centroids_sf,
-    project_to_grid_crs(subset_lake_centroids_sf, grid_cells)
-  ),
-
-  # Pull the tile ids, for mapping
-  tar_target(
-    grid_tile_ids,
-    grid_tiles_sf$tile_no
-  ),
+  # Reproject lake centroids to crs of grid cells
+  tar_target(query_lake_centroids_sf, project_to_crs(subset_lake_centroids_sf, grid_cells_ogr)),
 
   # Get cells associated with each tile
-  # MAPPING over grid tile ids
+  # MAPPING over grid tiles
   tar_target(
     tile_cells_sf,
-    get_tile_cells(grid_cell_centroids_sf, grid_cells_sf, grid_tiles_sf, grid_tile_ids),
-    pattern = map(grid_tile_ids),
-    iteration = 'list'
-  ),
-
-  # Get mapping of which lakes are in which cells (w/ no spatial info)
-  tar_target(
-    lake_cell_xwalk,
-    get_lake_cell_xwalk(query_lake_centroids_sf, tile_cells_sf),
-    pattern = map(tile_cells_sf),
-    iteration = 'list'
+    get_tile_cells(grid_cell_centroids_sf, grid_cells_sf, grid_tiles_sf),
+    pattern = map(grid_tiles_sf)
   ),
 
   # Spatially filter cells within each tile to only those cells
   # that contain lake centroids
   tar_target(
     query_cells_sf,
-    keep_cells_with_lakes(tile_cells_sf, query_lake_centroids_sf),
-    pattern = map(tile_cells_sf),
-    iteration = 'list'
+    get_query_cells(tile_cells_sf, query_lake_centroids_sf),
+    pattern = map(tile_cells_sf)
   ),
 
-  # Spatially filter centroids to those that fall within
-  # tile cells that have lakes
+  # Get mapping of which lakes are in which cells (w/ no spatial info)
+  # (for use later so that we know what data to pull for each lake)
+  tar_target(
+    lake_cell_xwalk,
+    get_lake_cell_xwalk(query_lake_centroids_sf, tile_cells_sf),
+    pattern = map(tile_cells_sf)
+  ),
+
+  # Spatially filter cell centroids to those that fall within
+  # tile cells that have lakes (since we want to use cell
+  # centroids in query, not cell polygons)
   tar_target(
     query_cell_centroids_sf,
     get_query_centroids(query_cells_sf, grid_cell_centroids_sf),
-    pattern = map(query_cells_sf),
-    iteration = 'list'
+    pattern = map(query_cells_sf)
   ),
 
-  # Convert the query grid cell centroids into a geoknife-ready format
-  # TODO - what projection does the request need to be in?
-  # returned proj4string from simplegeom is incorrect
-  # may need to reproject query cell centroids to EPSG 4326 first?
+  # MAP QUERY
+  # For now, mapping over tiles, and saving empty file if
+  # tiles do not contain any cells w/ lakes
+  # alternatively, can manually slice using branch indices
+  # pattern = slice(query_cells_sf, index = c(37,47,48)
+  # Get get ids of tiles with lakes with unique(lake_cell_xwalk$tile_no)
+  # matches branch indices. Unfortunately seems like you can't use
+  # a predefined vector to supply the indices to slice()
   tar_target(
-    query_centroids_geoknife,
-    sf_pts_to_simplegeom(query_cell_centroids_sf),
-    pattern = map(query_cell_centroids_sf),
-    iteration = 'list'
+    query_map_png,
+    map_query(
+      out_file_template = '7_drivers_munge/out/query_map_tileXX.png',
+      lake_centroids = query_lake_centroids_sf,
+      grid_tiles = grid_tiles_sf,
+      grid_cells = grid_cells_sf,
+      cells_w_lakes = query_cells_sf
+    ),
+    pattern = map(query_cells_sf),
+    format='file'
   ),
 
-  # TODO: make this parameterized/branched. Right now, just doing
-  # a single GCM, but will want to parameterize to each of them +
-  # handle chunks of lakes. See this config file example for an example:
-  # https://github.com/USGS-R/necsc-lake-modeling/blob/a81a0e7ed6ede66253f765b42568a4d39a5dccc2/configs/ACCESS_config.yml
+  # BUILD QUERY
+  # Define list of GCMs
+  tar_target(
+    gcm_names,
+    c('ACCESS', 'GFDL', 'CNRM', 'IPSL', 'MRI', 'MIROC')
+  ),
+  # TODO: Address issue that not all tiles contain cells that contain lakes
+  # so don't need to pull for each tile, so therefore don't need to generate file
+  # in each branch. For now, writing empty file, but means final vector
+  # contains placeholder filename for any tile that doesn't contain cells w/ lakes.
+  # Ideally, would only return filename if data is downloaded, but not sure
+  # targets would accept that for a file target.
   tar_target(
     gcm_data_raw_feather,
     download_gcm_data(
-      out_file = "7_drivers_munge/tmp/7_GCM_GFDL_raw.feather",
-      query_geom = query_centroids_geoknife,
-      query_url = "https://cida.usgs.gov/thredds/dodsC/notaro_GFDL_1980_1999",
+      out_file_template = "7_drivers_munge/tmp/7_GCM_GCMname_tileXX_raw.feather",
+      query_geom = query_cell_centroids_sf,
+      query_url_template = "https://cida.usgs.gov/thredds/dodsC/notaro_GCMname_1980_1999",
+      gcm_name = gcm_names,
       # Data definitions: https://cida.usgs.gov/thredds/ncss/notaro_GFDL_2040_2059/dataset.html
       # Can't use `mrso` until https://github.com/USGS-R/geoknife/issues/399 is fixed, but we shouldn't need it
       query_vars = c("evspsbl", "hfss"),
       query_dates = c('1999-01-01', '1999-01-15')
     ),
+    pattern = cross(query_cell_centroids_sf, gcm_names),
     format = "file"
   ),
-
-  tar_target(
-    query_map_png,
-    map_query(
-      out_file = '7_drivers_munge/out/query_map.png',
-      centroids_sf = query_lake_centroids_sf
-    ),
-    format='file'
-  ),
+  # TODO - munge data
+  #
+  # Get list of final output files
   tar_target(
     gcm_files_out,
-    c(gcm_data_raw_txt, query_map_png)
+    c(gcm_data_raw_feather, query_map_png)
   )
 )
 
