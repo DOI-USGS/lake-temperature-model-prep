27885bc5b3542c47aaa7d81813db911b063beb17
Jordan S. Read <jread@usgs.gov>
2021-12-06 07:29:08 -0600

    Update docs in `make_file_stale()`

--- lib/src/generic_utils.R
+++ lib/src/generic_utils.R
@@ -6,6 +6,8 @@
 #'
 #' @details this function is used to keep a dependency stale always, assuming this function is called whenever
 #' that file is referenced and there is at least a paired target use of the file.
+#' The file is kept stale by writing the current time (with timezone offset) and a random number. The random
+#' number is used because this function could be called rapidly several times within the same second.
 make_file_stale <- function(filepath){
-  cat(file = filepath, paste0(format(Sys.time(), '%m/%d/%y %H:%M:%S; random ID: '), sample(1E6, 1)))
+  cat(file = filepath, paste0(format(Sys.time(), '%m/%d/%y %H:%M:%S %z; random ID: '), sample(1E6, 1)))
 }
