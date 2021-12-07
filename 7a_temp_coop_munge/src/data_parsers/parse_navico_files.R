a0a4352e643d29ccf7231873719b0a6c271aea45
Julie Padilla <padilla410@gmail.com>
2021-12-07 14:16:48 -0500

    prep files to add navico data

---
+++ 7a_temp_coop_munge/src/data_parsers/parse_navico_files.R
@@ -0,0 +1,36 @@
+parse_navico_files <- function(inind, outind){
+  
+  infile <- sc_retrieve(inind, remake_file = '6_temp_coop_fetch_tasks.yml')
+  outfile <- as_data_file(outind)
+  
+  # standardize column types - required because a subset of 
+  # numeric columns contain text. Text in data columns will be coerced to `NA`
+  cols <- c(rep('text', 2), # state and wb name
+            rep('numeric', 18), # all data columns and some metadata
+            rep('date', 2)) # dates
+
+  # read in data: identify sheets in data, read in raw data by sheet, and combine into one data set
+  raw_sheets <- readxl::excel_sheets(infile)
+  ls_raw <- lapply(raw_sheets, function(x){
+    out <- read_xlsx(infile, sheet = x, col_types = cols)
+    return(out)
+  })
+  raw <- bind_rows(ls_raw)
+
+  # clean data
+  clean <- raw %>% 
+    filter(!is.na(`Hour (UTC)`)) %>% # remove temporally aggregated data
+    mutate(date_complete = 
+             lubridate::ymd_h(paste(Year, Month, Day, `Hour (UTC)`, sep = ' ')),
+           DateTime = lubridate::ymd(paste(Year, Month, Day, sep = ' ')),
+           TimeZone = "UTC",
+           depth = 0,  # assumed surface samples
+           id = paste("Navico_", MapWaterbodyId, sep = '')) %>%
+    mutate(time = format(date_complete, "%H:%M")) %>% 
+    select(DateTime, time, TimeZone, depth, temp = AveWaterTempC, id) 
+
+  # save data  
+  saveRDS(object = clean, file = outfile)
+  sc_indicate(ind_file = outind, data_file = outfile)
+  
+}
