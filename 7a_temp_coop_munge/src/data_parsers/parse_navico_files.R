4fb2d85b376695f169c277e2df34d263522a29ad
Julie Padilla <padilla410@gmail.com>
2021-12-08 10:15:33 -0500

    debugging dplyr:;select in navico parser

--- 7a_temp_coop_munge/src/data_parsers/parse_navico_files.R
+++ 7a_temp_coop_munge/src/data_parsers/parse_navico_files.R
@@ -1,9 +1,9 @@
 parse_navico_files <- function(inind, outind){
-  
+
   infile <- sc_retrieve(inind, remake_file = '6_temp_coop_fetch_tasks.yml')
   outfile <- as_data_file(outind)
-  
-  # standardize column types - required because a subset of 
+
+  # standardize column types - required because a subset of
   # numeric columns contain text. Text in data columns will be coerced to `NA`
   cols <- c(rep('text', 2), # state and wb name
             rep('numeric', 18), # all data columns and some metadata
@@ -18,19 +18,19 @@ parse_navico_files <- function(inind, outind){
   raw <- bind_rows(ls_raw)
 
   # clean data
-  clean <- raw %>% 
-    filter(!is.na(`Hour (UTC)`)) %>% # remove temporally aggregated data
-    mutate(date_complete = 
+  clean <- raw %>%
+    dplyr::filter(!is.na(`Hour (UTC)`)) %>% # remove temporally aggregated data
+    dplyr::mutate(date_complete =
              lubridate::ymd_h(paste(Year, Month, Day, `Hour (UTC)`, sep = ' ')),
            DateTime = lubridate::ymd(paste(Year, Month, Day, sep = ' ')),
            TimeZone = "UTC",
            depth = 0,  # assumed surface samples
            id = paste("Navico_", MapWaterbodyId, sep = '')) %>%
-    mutate(time = format(date_complete, "%H:%M")) %>% 
-    select(DateTime, time, TimeZone, depth, temp = AveWaterTempC, id) 
+    dplyr::mutate(time = format(date_complete, "%H:%M")) %>%
+    dplyr::select(DateTime, time, TimeZone, depth, temp = AveWaterTempC, id)
 
-  # save data  
+  # save data
   saveRDS(object = clean, file = outfile)
   sc_indicate(ind_file = outind, data_file = outfile)
-  
+
 }
