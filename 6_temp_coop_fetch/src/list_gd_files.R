1defb29f20db3e527496ea37dd8adf0f72ffa850
Jordan S. Read <jread@usgs.gov>
2021-12-09 16:42:51 -0600

    feels like a bad idea!

--- 6_temp_coop_fetch/src/list_gd_files.R
+++ 6_temp_coop_fetch/src/list_gd_files.R
@@ -1,8 +1,21 @@
 # function to retrieve list of files from google drive
 
-files_in_drive <- function(out_ind, gd_path, dummy_date) {
+#' @param trigger_file specify a filepath to keep this target stale.
+#' Omit (NULL) to avoid rebuilds
+#' @param trigger_wait number of minutes to wait between making this stale again
+#'
+#'
+#' IAAAAA should we be doing this? probably not
+files_in_drive <- function(out_ind, gd_path, trigger_file = NULL, trigger_wait) {
 
-  updater <- dummy_date
+  if (!is.null(trigger_file)){
+    time_since_stale <- readLines(trigger_file, n =  1) %>%
+      strsplit(';') %>% {.[[1]][1]} %>%
+      {Sys.time() - as.POSIXct(.)} %>% as.numeric()
+    if (time_since_stale > trigger_wait)
+      make_file_stale(trigger_file)
+  }
+
   gd_files <- drive_ls(path = gd_path)
 
   saveRDS(gd_files, file = as_data_file(out_ind))
