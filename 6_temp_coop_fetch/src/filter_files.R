6ea193b6d0e594b54765e96f7cbabe80056c68c1
Jordan S. Read <jread@usgs.gov>
2021-12-09 19:45:19 -0600

    #231 coop GD file list as 30min max always stale

--- 6_temp_coop_fetch/src/filter_files.R
+++ 6_temp_coop_fetch/src/filter_files.R
@@ -1,8 +1,15 @@
-filter_coop_all <- function(in_ind) {
+#' @param trigger_file specify a filepath to keep this target stale.
+#' Omit (NULL) to avoid rebuilds
+filter_coop_all <- function(in_ind, trigger_file) {
 
   # this is just a filter to exclude files in gd that
   # should eventually be moved/cleaned up
   # for now, just keep all .xlsx, .txt, .csv, .accdb
+
+  if (!is.null(trigger_file)){
+    make_file_stale(trigger_file)
+  }
+
 
   gd_files <- readRDS(sc_retrieve(in_ind))
   filenames <- gd_files$name
