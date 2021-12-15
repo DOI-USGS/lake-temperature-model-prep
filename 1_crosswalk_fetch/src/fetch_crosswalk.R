1ba7d933f7323be2b96f437e9e23c6e4b874f7fb
Jordan S. Read <jread@usgs.gov>
2021-12-15 07:50:45 -0600

    Wasn't happy with having depths and non-depth sites together

--- 1_crosswalk_fetch/src/fetch_crosswalk.R
+++ 1_crosswalk_fetch/src/fetch_crosswalk.R
@@ -208,58 +208,17 @@ fetch_micorps_sites <- function(ind_file) {
 
 }
 
-#' @param depth_ind indicator file for Navico depths file
-#' @param temp_ind indicator file for Navico water temperature lakes
-#'
-#' @details to create a full Navico crosswalk we need information from both of
-#' these files. Including `temp_ind` is a bit odd, because it exists in the coop
-#' fetch step and not all contributors to this pipeline have built that
-#'
-#' This function will error if the `sc_retrieve` for temp_ind doesn't have
-#' access to 6_temp_coop_fetch_tasks.yml, which isn't checked into git
-fetch_navico_points <- function(out_ind, depth_ind, temp_ind) {
-
-  if (!file.exists('6_temp_coop_fetch_tasks.yml')){
-    stop('in order to access ', temp_ind, ' must have built 6_temp_coop_fetch_tasks.yml', call. = FALSE)
-  }
+fetch_navico_points <- function(out_ind, csv_ind) {
   outfile <- as_data_file(out_ind)
 
-  navico_depth_data <- scipiper::sc_retrieve(depth_ind) %>%
+  navico_data <- scipiper::sc_retrieve(csv_ind) %>%
     read_csv(col_types = 'dcccdddddi', na='NULL')
-
-  navico_temp_file <- scipiper::sc_retrieve(temp_ind, remake_file = '6_temp_coop_fetch_tasks.yml')
-  # temp doesn't use the default location for `gd_get()` for this target, which would be `getters.yml`,
-  # so setting this arg to the task table yaml:
-  navico_temp_points_sf <- readxl::excel_sheets(navico_temp_file) %>%
-    lapply(function(x){
-      read_xlsx(navico_temp_file, sheet = x, col_types = c(rep('text', 2), # state and wb name
-                                                        rep('numeric', 18), # all data columns and some metadata
-                                                        rep('date', 2)))
-    }) %>%
-    bind_rows() %>%
-    filter(!is.na(MapWaterbodyId), !is.na(WaterbodyLon), !is.na(WaterbodyLat)) %>% group_by(MapWaterbodyId) %>%
-    summarize(
-      site_id = sprintf("Navico_%s", dplyr::first(MapWaterbodyId)),
-      lat = dplyr::first(WaterbodyLat),
-      lon = dplyr::first(WaterbodyLon)) %>%
-    dplyr::select(site_id, lon, lat) %>%
-    st_as_sf(coords = c('lon','lat'), crs=4326) %>%
-    mutate(SocialMapMaxDepthMeters = NA, SocialMapMeanDepthMeters = NA)
 
   # convert data to sf object and save as rds
   # rename waterbody id to site id, so that rds can later be passed to `crosswalk_points_in_poly`
   # which expects input dataframe to have 'site_id' column
-  navico_depth_points_sf <- st_as_sf(navico_depth_data, coords = c('CenterLong','CenterLat'), crs=4326) %>%
+  navico_points_sf <- st_as_sf(navico_data, coords = c('CenterLong','CenterLat'), crs=4326) %>%
     mutate(site_id = sprintf("Navico_%s", MapWaterbody_ID), .keep="unused", .before=1) %>%
-    dplyr::select(site_id, SocialMapMaxDepthMeters, SocialMapMeanDepthMeters)
-
-
-  bind_rows(navico_temp_points_sf, navico_depth_points_sf) %>%
-    group_by(site_id) %>% summarize(
-      # when lakes are in both, use the location of the depth file, which is more likely to be in a central location:
-      site_id = dplyr::last(site_id),
-      SocialMapMaxDepthMeters = dplyr::last(SocialMapMaxDepthMeters),
-      SocialMapMeanDepthMeters = dplyr::last(SocialMapMeanDepthMeters)) %>%
     saveRDS(file = outfile)
 
   gd_put(out_ind, outfile)
