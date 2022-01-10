3d638506af6bb107cfe08ee0f9fa63e6753cfe7f
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-01-10 17:26:07 -0600

    Pass instance names as character vector

--- 7_drivers_munge/src/GCM_driver_nc_utils.R
+++ 7_drivers_munge/src/GCM_driver_nc_utils.R
@@ -6,24 +6,18 @@
 #' @param nc_file netcdf file name output
 #' @param gcm_raw_files vector of feather file paths, one for each tile with data for the current GCM.
 #' @param dim_time_input vector of GCM driver data dates
-#' @param dim_cell_input vector of all GCM driver grid cells (whether or not data was pulled)
 #' @param vars_info variables and descriptions to store in NetCDF
-#' @param crs_info of GCMs - NEED TO FIGURE OUT HOW TO ADD TO NETCDF
+#' @param crs_info of GCMs - added to NetCDF as global attributes
 #' @param spatial_info cell_nos, x indices, y indices, and projected coordinates of grid cell centroids
 #' @param global_att global attribute description for the observations (e.g. notaro_ACCESS_1980_1999)
 #' @param overwrite T/F if the nc file should be overwritten if it exists already
-generate_gcm_nc <- function(nc_file, gcm_raw_files, dim_time_input, dim_cell_input,
+generate_gcm_nc <- function(nc_file, gcm_raw_files, dim_time_input,
                             vars_info, crs_info, spatial_info, global_att, overwrite = TRUE) {
 
   # Delete nc outfile if it exists already
   if (file.exists(nc_file)) {
     unlink(nc_file)
   }
-
-  # Use `ncdfgeom` to write out the NetCDF DSG file
-  # See example at https://github.com/jread-usgs/lakesurf-data-release/blob/main/src/ncdf_utils.R#L66-L76
-  # And more discussion in https://github.com/USGS-R/lake-temperature-model-prep/issues/252
-  # ncdfgeom::write_timeseries_dsg(nc_file)
 
   # Read in all data for that GCM and pivot to long format
   gcm_data_long <- purrr::map_df(gcm_raw_files, function(gcm_raw_file) {
@@ -32,7 +26,8 @@ generate_gcm_nc <- function(nc_file, gcm_raw_files, dim_time_input, dim_cell_inp
     pivot_longer(cols = -c(time, cell), names_to = "variable", values_to = "value") %>%
     arrange(cell)
 
-  # pull vector of gcm cell numbers to use as instance names
+  # pull vector of unique cell numbers
+  # (will later be converted to characters and used as instance names)
   gcm_cells <- unique(gcm_data_long$cell)
 
   # get cell coordinates
@@ -48,9 +43,13 @@ generate_gcm_nc <- function(nc_file, gcm_raw_files, dim_time_input, dim_cell_inp
 
   data_time_units <- "days since 1970-01-01 00:00:00"
   data_attributes <- list('title' = global_att)
+  # add grid parameters to list of global attributes
+  data_attributes <- append(data_attributes, crs_info)
   data_coordvar_long_names <- list(instance = "identifier for GCM grid cell", time = "date",
                               lat = "latitude of GCM grid cell centroid", lon = "longitude of GCM grid cell centroid",
                               alt = "NULL")
+
+  # message(sprintf('nc_file: %s', nc_file))
 
   # Loop over data variables to populate netCDF using ncdfgeom::write_timeseries_dsg()
   for (variable in vars_info$var_name) {
@@ -69,7 +68,9 @@ generate_gcm_nc <- function(nc_file, gcm_raw_files, dim_time_input, dim_cell_inp
     var_add_to_existing <- ifelse(file.exists(nc_file), TRUE, FALSE)
 
     # # messages to check dims
+    # message('----------------------')
     # message(sprintf('variable: %s', variable))
+    # message(sprintf('add to existing: %s', var_add_to_existing))
     # message(sprintf('cell length: %s', length(gcm_cells)))
     # message(sprintf('lat length: %s', length(cell_lats)))
     # message(sprintf('lon length: %s', length(cell_lons)))
@@ -77,8 +78,6 @@ generate_gcm_nc <- function(nc_file, gcm_raw_files, dim_time_input, dim_cell_inp
     # message(sprintf('ncol var data: %s', ncol(var_data)))
     # message(sprintf('nrow var data: %s', nrow(var_data)))
     # message(sprintf('length data units: %s', length(var_data_unit)))
-
-    # browser()
 
     # write this variable to the netcdf file:
     # ncdfgeom::write_timeseries_dsg() function documentation:
@@ -104,7 +103,7 @@ generate_gcm_nc <- function(nc_file, gcm_raw_files, dim_time_input, dim_cell_inp
     # variables will be added to the existing file. See details for more.
     # @param overwrite boolean error if file exists.
     ncdfgeom::write_timeseries_dsg(nc_file,
-                         instance_names = gcm_cells,
+                         instance_names = as.character(gcm_cells),
                          lats = cell_lats,
                          lons = cell_lons,
                          alts = NA,
@@ -117,7 +116,7 @@ generate_gcm_nc <- function(nc_file, gcm_raw_files, dim_time_input, dim_cell_inp
                          attributes = data_attributes,
                          coordvar_long_names = data_coordvar_long_names,
                          add_to_existing = var_add_to_existing,
-                         overwrite = TRUE)
+                         overwrite = overwrite)
   }
 
   return(nc_file)
